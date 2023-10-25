7762d946fb8a84a2adda6fce03083be8de316736
miguel viana <mvalcantara14@gmail.com>
2023-10-25 12:18:49 -0300

    script inimigo

--- Refugio/Assets/Scripts/Enemy.cs
+++ Refugio/Assets/Scripts/Enemy.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform Player;
+    public float speed;
+    public float stoppingDistance;
+    public LayerMask groundMask;
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Mathf.Abs(transform.position.x - Player.position.x) <= stoppingDistance)
+        {
+            // Set Y velocity to 0 to prevent movement in Y axis
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+        else
+        {
+            // Set the X velocity based on the direction of the player
+            float x = (Player.position.x > transform.position.x) ? speed : -speed;
+            rb.velocity = new Vector2(x, rb.velocity.y);
+        }
     }
 }
--- Refugio/Assets/Scripts/Player.cs
+++ Refugio/Assets/Scripts/Player.cs
@@ -21,4 +21,21 @@ public class Player : MonoBehaviour
             RB.velocity = new Vector2(RB.velocity.x, 7f);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            // Call a method to handle player death
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // You can implement your own logic for player death here
+        // For example, you might want to play an animation or respawn the player
+        Debug.Log("Player has died");
+        // Add your own code for handling player death here
+    }
 }
