0ef3eaad56dfd98abbd505355bfd70f6396e6b2a
Eduardo Schmidt Schultz <schmidt_eduardo@outlook.com>
2023-10-31 11:58:03 -0300

    Sistema de vida OK

--- Refugio/Assets/Scripts/Player.cs
+++ Refugio/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -15,11 +16,21 @@ public class Player : MonoBehaviour
     public bool onGround;
     public float forcaPulo;
     public float veloPlayer;
+    //Vida
+    public int health;
+    public Image heart1;
+    public Image heart2;
+    public Image heart3;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
 
     private void Start()
     {
         RB = GetComponent<Rigidbody2D>();
+        heart3.sprite = fullHeart;
+        heart2.sprite = fullHeart;
+        heart1.sprite = fullHeart;
     }
 
     private void Update()
@@ -47,7 +58,37 @@ public class Player : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
+        {   
+            health = health - 1;
+
+            if (health == 2)
+            {
+                heart3.sprite = fullHeart;
+                heart2.sprite = fullHeart;
+                heart1.sprite = emptyHeart;
+            }
+            if (health == 1)
+            {
+                heart3.sprite = fullHeart;
+                heart2.sprite = emptyHeart;
+                heart1.sprite = emptyHeart;
+            }
+            if (health == 0)
+            {
+                heart3.sprite = emptyHeart;
+                heart2.sprite = emptyHeart;
+                heart1.sprite = emptyHeart;
+                isAlive = false;
+                Time.timeScale = 0;
+                GameOverPanel.SetActive(true);
+            }
+        }
+
+        if (collision.gameObject.CompareTag("Hell"))
         {
+            heart3.sprite = emptyHeart;
+            heart2.sprite = emptyHeart;
+            heart1.sprite = emptyHeart;
             isAlive = false;
             Time.timeScale = 0;
             GameOverPanel.SetActive(true);
