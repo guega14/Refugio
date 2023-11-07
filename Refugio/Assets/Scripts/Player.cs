c6d3fab4aaf9b13be56bd4076205d2c963752381
Eduardo Schmidt Schultz <schmidt_eduardo@outlook.com>
2023-11-07 07:52:04 -0300

    Mudança de fase e nuvem adicionadas

--- Refugio/Assets/Scripts/Player.cs
+++ Refugio/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -92,6 +93,10 @@ public class Player : MonoBehaviour
             isAlive = false;
             Time.timeScale = 0;
             GameOverPanel.SetActive(true);
+        }
+        if (collision.gameObject.CompareTag("Heven"))
+        {
+            SceneManager.LoadScene("Fase2");
         }
     }
 
